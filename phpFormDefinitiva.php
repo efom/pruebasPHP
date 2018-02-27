b0c7a52795b7ca272c216b296ea5ca17db0772bf
Elkin Faber <ef331@misena.edu.co>
2018-02-27 11:16:57 -0500

    Nuevo archivo

---
+++ phpFormDefinitiva.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Calculo Definitiva</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<h1>Ejercicio que calcula la definitiva de una materia con cuatro notas</h1>
+	<form action="phpCalculoDefinitiva.php" method="POST" accept-charset="utf-8">
+		Introduzca su nombre:
+		<input type="text" name="nombre" value="" placeholder=""><br>
+		Introduzca la nota 1:
+		<input type="text" name="nota1" value="" placeholder=""><br>
+		Introduzca la nota 2:
+		<input type="text" name="nota2" value="" placeholder=""><br>
+		Introduzca la nota 3:
+		<input type="text" name="nota3" value="" placeholder=""><br>
+		Introduzca la nota 4:
+		<input type="text" name="nota4" value="" placeholder=""><br>
+		<input type="submit" name="definitiva" value="Definitiva">
+	</form>
+</body>
+</html>
